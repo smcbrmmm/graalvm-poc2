81d6fa813efcba7b4fb68bfbbf987a62da72a485
smcbrmmm <samutch19@gmail.com>
2023-11-13 16:23:38 +0700

    first commit

---
+++ src/main/java/com/poc/graalvmpoc/GraalvmPocApplication.java
@@ -0,0 +1,13 @@
+package com.poc.graalvmpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalvmPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalvmPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/graalvmpoc/GraalvmPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.graalvmpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
